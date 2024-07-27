24e7bb6580cf724021b69e67ef6be089d3095caa
Monarch789 <muhammadarhamali003@gmail.com>
2024-07-27 16:37:53 +0500

    Added level select option

---
+++ Assets/Scripts/UI Scripts/LevelSelectUI.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectUI : MonoBehaviour{
+
+    //Reference of non level buttons
+    [SerializeField] private Button BackButton;
+    [SerializeField] private Button TutorialButton;
+
+    //refernce of level buttons
+    [SerializeField] private Button Level1Button;
+    [SerializeField] private Button Level2Button;
+    [SerializeField] private Button Level3Button;
+    [SerializeField] private Button Level4Button;
+    [SerializeField] private Button Level5Button;
+
+
+    //aniamtor 
+    private Animator animator;
+
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+
+        BackButton.onClick.AddListener(() => {
+            Hide();
+        });
+
+        TutorialButton.onClick.AddListener(() => {
+            Loader.LoadScene(Loader.GameScenes.SampleScene);
+        });
+
+        //load respective scenes
+        Level1Button.onClick.AddListener(() => {
+            Loader.LoadScene(Loader.GameScenes.SampleScene);
+        });
+        Level2Button.onClick.AddListener(() => {
+            Loader.LoadScene(Loader.GameScenes.SampleScene);
+        });
+        Level3Button.onClick.AddListener(() => {
+            Loader.LoadScene(Loader.GameScenes.SampleScene);
+        });
+        Level4Button.onClick.AddListener(() => {
+            Loader.LoadScene(Loader.GameScenes.SampleScene);
+        });
+        Level5Button.onClick.AddListener(() => {
+            Loader.LoadScene(Loader.GameScenes.SampleScene);
+        });
+
+
+    }
+
+    private void Start() {
+        Hide();
+
+        MainMenu.Instance.OnPlayButtonClick += MainMenu_OnPlayButtonClick;
+    }
+
+    private void MainMenu_OnPlayButtonClick(object sender, System.EventArgs e) {
+        Show();
+
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
+    }
+    private void Show() {
+        gameObject.SetActive(true);
+
+        animator.SetTrigger("OnPlayClick");
+    }
+
+
+}
--- Assets/Scripts/UI Scripts/MainMenu.cs
+++ Assets/Scripts/UI Scripts/MainMenu.cs
@@ -1,16 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour{
+    //Singleton
+    public static MainMenu Instance {  get; private set; }
+
+    //event to send level select to show itself
+    public event EventHandler OnPlayButtonClick;
 
     [SerializeField] private Button PlayButton;
     [SerializeField] private Button QuitButton;
 
     private void Awake() {
+        Instance = this;
+
         PlayButton.onClick.AddListener(() => {
-            Loader.LoadScene(Loader.GameScenes.SampleScene);
+
+            OnPlayButtonClick?.Invoke(this, EventArgs.Empty);
+
         });
 
         QuitButton.onClick.AddListener(() => {
